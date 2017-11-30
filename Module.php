b6d774b105284a42f4a1b9c124d152e91c72e7ed
meyergru <github_email@congenio.de>
2017-11-30 10:53:21 +0100

    Update Module.php
    Compatability with Yii2 2.13

--- Module.php
+++ Module.php
@@ -8,7 +8,7 @@ use  \array_key_exists;
 
 /**
  * For example,
- * 
+ *
  * ```php
  * 'oauth2' => [
  *     'class' => 'filsh\yii2\oauth2server\Module',
@@ -32,22 +32,22 @@ use  \array_key_exists;
 class Module extends \yii\base\Module
 {
     const VERSION = '2.0.0';
-    
+
     /**
      * @var array Model's map
      */
     public $modelMap = [];
-    
+
     /**
      * @var array Storage's map
      */
     public $storageMap = [];
-    
+
     /**
      * @var array GrantTypes collection
      */
     public $grantTypes = [];
-    
+
     /**
      * @var array server options
      */
@@ -57,7 +57,7 @@ class Module extends \yii\base\Module
      * @var string name of access token parameter
      */
     public $tokenParamName;
-    
+
     /**
      * @var type max access lifetime
      */
@@ -66,7 +66,7 @@ class Module extends \yii\base\Module
      * @var whether to use JWT tokens
      */
     public $useJwtToken = false;//ADDED
-    
+
     /**
      * @inheritdoc
      */
@@ -75,18 +75,18 @@ class Module extends \yii\base\Module
         parent::init();
         $this->registerTranslations();
     }
-    
+
     /**
      * Gets Oauth2 Server
-     * 
+     *
      * @return \filsh\yii2\oauth2server\Server
      * @throws \yii\base\InvalidConfigException
      */
     public function getServer()
     {
-        if(!$this->has('server')) {
+        if(!$this->has('oauth2server', true)) {
             $storages = [];
-            
+
             if($this->useJwtToken)
             {
                 if(!array_key_exists('access_token', $this->storageMap) || !array_key_exists('public_key', $this->storageMap)) {
@@ -100,11 +100,11 @@ class Module extends \yii\base\Module
                 \Yii::$container->clear('access_token'); //remove old definition
                 \Yii::$container->set('access_token', $this->storageMap['access_token']);
             }
-            
+
             foreach(array_keys($this->storageMap) as $name) {
                 $storages[$name] = \Yii::$container->get($name);
             }
-            
+
             $grantTypes = [];
             foreach($this->grantTypes as $name => $options) {
                 if(!isset($storages[$name]) || empty($options['class'])) {
@@ -120,7 +120,7 @@ class Module extends \yii\base\Module
                 $instance = $reflection->newInstanceArgs($config);
                 $grantTypes[$name] = $instance;
             }
-            
+
             $server = \Yii::$container->get(Server::className(), [
                 $this,
                 $storages,
@@ -133,30 +133,30 @@ class Module extends \yii\base\Module
                 $grantTypes
             ]);
 
-            $this->set('server', $server);
+            $this->set('oauth2server', $server);
         }
-        return $this->get('server');
+        return $this->get('oauth2server');
     }
-    
+
     public function getRequest()
     {
-        if(!$this->has('request')) {
-            $this->set('request', Request::createFromGlobals());
+        if(!$this->has('oauth2request', true)) {
+            $this->set('oauth2request', Request::createFromGlobals());
         }
-        return $this->get('request');
+        return $this->get('oauth2request');
     }
-    
+
     public function getResponse()
     {
-        if(!$this->has('response')) {
-            $this->set('response', new Response());
+        if(!$this->has('oauth2response', true)) {
+            $this->set('oauth2response', new Response());
         }
-        return $this->get('response');
+        return $this->get('oauth2response');
     }
 
     /**
      * Register translations for this module
-     * 
+     *
      * @return array
      */
     public function registerTranslations()
@@ -168,10 +168,10 @@ class Module extends \yii\base\Module
             ];
         }
     }
-    
+
     /**
      * Translate module message
-     * 
+     *
      * @param string $category
      * @param string $message
      * @param array $params
